bdf95e6c4e912d6e621caa81dabb6aafffc4bdde
Unknown <tabra12@freeuni.edu.ge>
2018-05-04 11:57:34 +0400

    Initial vue+express+vuetify project

---
+++ nuxt.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'starter',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+      }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: ['~/assets/css/main.css', '~/assets/style/app.styl'],
+
+  plugins: ['~/plugins/vuetify'],
+  /*
+  ** Add axios globally
+  */
+  build: {
+    vendor: ['axios', '~/plugins/vuetify.js'],
+    extractCSS: true,
+    /*
+    ** Run ESLINT on save
+    */
+    extend (config, ctx) {
+      if (ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
